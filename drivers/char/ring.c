0c8e5aba4ef220d9ec9d4b7d6ef779676f6da986
Piotr Zalewski <pZ010001011111@proton.me>
2025-03-29 23:39:19 +0100

    Z1: add 3 more buffers to ring char dev

--- drivers/char/ring.c
+++ drivers/char/ring.c
@@ -10,44 +10,61 @@
 
 #define BUFFERSIZE 1024
 
-static char *buffer;
-int buffercount;
-int start,end;
-int usecount;
-struct semaphore sem=MUTEX;
+#define DEVS_COUNT 4
+
+struct RingDev {
+	char *buffer;
+	int buffercount;
+	int start;
+	int end;
+	int usecount;
+	struct semaphore sem;
+};
+
+static struct RingDev ring_devs[DEVS_COUNT];
+
 struct wait_queue *read_queue,*write_queue; 
+
+static struct RingDev *get_dev(struct inode *inode)
+{
+	int dev_nr = MINOR(inode->i_rdev);
+	return ring_devs + dev_nr;
+}
 
 int ring_open(struct inode *inode,struct file *file) 
 {
+	struct RingDev *dev = get_dev(inode);
 	
-	down(&sem);
-	usecount++;
-	if (usecount == 1) {
-	        
+	down(&dev->sem);
+	dev->usecount++;
+	if (dev->usecount == 1) {
 	        // kmalloc moze uspic proces - uwaga na synchronizacje
-		buffer=kmalloc(BUFFERSIZE,GFP_KERNEL);
-		buffercount=start=end=0;
+		dev->buffer=kmalloc(BUFFERSIZE,GFP_KERNEL);
+		dev->buffercount=dev->start=dev->end=0;
 	} 
-	up(&sem);
+	up(&dev->sem);
 	return 0;
 	
 }
 
 void ring_release(struct inode *inode,struct file *file) 
 {
-	usecount--;
-	if (usecount==0)
-		kfree(buffer);
+	struct RingDev *dev = get_dev(inode);
+
+	dev->usecount--;
+	if (dev->usecount==0)
+		kfree(dev->buffer);
 }
 
 int ring_read(struct inode *inode,struct file *file,char *pB,int count)
 {
+	struct RingDev *dev = get_dev(inode);
 
 	int i;
 	char tmp;
 	for(i=0;i<count;i++) {
-		while (buffercount==0) {
-			if (usecount==1)
+		while (dev->buffercount==0) {
+			if (dev->usecount==1)
 				return i;
 	
 			interruptible_sleep_on(&read_queue);
@@ -58,11 +75,11 @@ int ring_read(struct inode *inode,struct file *file,char *pB,int count)
 			}
 		}
 		
-		tmp=buffer[start];
-		start++;
-		if (start==BUFFERSIZE)
-			start=0;
-		buffercount--;
+		tmp=dev->buffer[dev->start];
+		dev->start++;
+		if (dev->start==BUFFERSIZE)
+			dev->start=0;
+		dev->buffercount--;
 		wake_up(&write_queue);
 		put_user(tmp,pB+i);
 	}		
@@ -71,11 +88,13 @@ int ring_read(struct inode *inode,struct file *file,char *pB,int count)
 
 int ring_write(struct inode *inode,struct file *file,const char *pB,int count)
 {
+	struct RingDev *dev = get_dev(inode);
+
 	int i;
 	char tmp;		
 	for(i=0;i<count;i++) {
 		tmp=get_user(pB+i);
-		while (buffercount==BUFFERSIZE) {
+		while (dev->buffercount==BUFFERSIZE) {
 			interruptible_sleep_on(&write_queue);
 			if (current->signal & ~current->blocked) {
 				if (i==0)
@@ -83,11 +102,11 @@ int ring_write(struct inode *inode,struct file *file,const char *pB,int count)
 				return i;
 			}
 		}
-		buffer[end]=tmp;
-		buffercount++;
-		end++;
-		if (end==BUFFERSIZE)
-			end=0;
+		dev->buffer[dev->end]=tmp;
+		dev->buffercount++;
+		dev->end++;
+		if (dev->end==BUFFERSIZE)
+			dev->end=0;
 		wake_up(&read_queue);
 	}
 	return count;
@@ -101,10 +120,20 @@ struct file_operations ring_ops = {
 
 #define RING_MAJOR 60
 
-void ring_init(void) {
+static void ring_devs_init()
+{
+	int i;
+	for (i = 0; i < DEVS_COUNT; ++i) {
+		ring_devs[i].usecount = 0;
+		ring_devs[i].sem = MUTEX;
+	}
+}
+
+void ring_init(void)
+{
 	init_waitqueue(&write_queue);
 	init_waitqueue(&read_queue);
-	usecount=0;
+	ring_devs_init();
 	register_chrdev(RING_MAJOR,"ring",&ring_ops);
 	printk("Ring device initialized\n");
 }
