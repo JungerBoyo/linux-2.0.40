ea48aacd8620b04fba77ecec2df704981583838b
Piotr Zalewski <pZ010001011111@proton.me>
2025-03-30 00:38:56 +0100

    Z2: add ioctl buffersize write command

--- drivers/char/ring.c
+++ drivers/char/ring.c
@@ -8,12 +8,13 @@
 
 
 
-#define BUFFERSIZE 1024
+#define DEFAULT_BUFFERSIZE 1024
 
 #define DEVS_COUNT 4
 
 struct RingDev {
 	char *buffer;
+	int buffersize;
 	int buffercount;
 	int start;
 	int end;
@@ -39,7 +40,7 @@ int ring_open(struct inode *inode,struct file *file)
 	dev->usecount++;
 	if (dev->usecount == 1) {
 	        // kmalloc moze uspic proces - uwaga na synchronizacje
-		dev->buffer=kmalloc(BUFFERSIZE,GFP_KERNEL);
+		dev->buffer=kmalloc(dev->buffersize,GFP_KERNEL);
 		dev->buffercount=dev->start=dev->end=0;
 	} 
 	up(&dev->sem);
@@ -75,11 +76,14 @@ int ring_read(struct inode *inode,struct file *file,char *pB,int count)
 			}
 		}
 		
+		down(&dev->sem);
 		tmp=dev->buffer[dev->start];
 		dev->start++;
-		if (dev->start==BUFFERSIZE)
+		if (dev->start==dev->buffersize)
 			dev->start=0;
 		dev->buffercount--;
+		up(&dev->sem);
+
 		wake_up(&write_queue);
 		put_user(tmp,pB+i);
 	}		
@@ -94,7 +98,7 @@ int ring_write(struct inode *inode,struct file *file,const char *pB,int count)
 	char tmp;		
 	for(i=0;i<count;i++) {
 		tmp=get_user(pB+i);
-		while (dev->buffercount==BUFFERSIZE) {
+		while (dev->buffercount==dev->buffersize) {
 			interruptible_sleep_on(&write_queue);
 			if (current->signal & ~current->blocked) {
 				if (i==0)
@@ -102,20 +106,50 @@ int ring_write(struct inode *inode,struct file *file,const char *pB,int count)
 				return i;
 			}
 		}
+		down(&dev->sem);
 		dev->buffer[dev->end]=tmp;
 		dev->buffercount++;
 		dev->end++;
-		if (dev->end==BUFFERSIZE)
+		if (dev->end==dev->buffersize)
 			dev->end=0;
+		up(&dev->sem);
 		wake_up(&read_queue);
 	}
 	return count;
 }
 
+#define IOCTL_BUFF_SIZE_WRITE 0x00
+
+int ring_ioctl(struct inode *inode,struct file *file, unsigned int cmd, unsigned long arg)
+{
+	struct RingDev *dev = get_dev(inode);
+	switch (cmd) {
+		case IOCTL_BUFF_SIZE_WRITE: {
+			if (dev->buffersize == arg)
+				return 0;
+
+			if (arg < 256 || arg > 16 * 1024)
+				return -EINVAL;
+
+			down(&dev->sem);
+			if (dev->usecount > 0) {
+				void *new_buffer = kmalloc(arg, GFP_KERNEL);
+				memcpy(new_buffer, dev->buffer, arg < dev->buffersize ? arg : dev->buffersize);
+				kfree(dev->buffer);
+				dev->buffer = new_buffer;
+			}
+			dev->buffersize = arg;
+			up(&dev->sem);
+		};
+	}
+
+	return -EINVAL;
+}
 
 struct file_operations ring_ops = {
 	read: ring_read, write:ring_write,
-	open:ring_open, release:ring_release};
+	open:ring_open, release:ring_release,
+	ioctl:ring_ioctl};
 
 
 #define RING_MAJOR 60
@@ -124,6 +158,7 @@ static void ring_devs_init()
 {
 	int i;
 	for (i = 0; i < DEVS_COUNT; ++i) {
+		ring_devs[i].buffersize = DEFAULT_BUFFERSIZE;
 		ring_devs[i].usecount = 0;
 		ring_devs[i].sem = MUTEX;
 	}
